44c11c4cecd9c82a985cf15cd78f3e32113e9e29
Tommyorrala17 <135489450+Tommyorrala17@users.noreply.github.com>
2023-12-11 19:56:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv,dotenv_values 
 
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+
+config = dotenv_values('.env')
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokedex.sqlite"
+
+# Vinculamos la base de datos con la app
+db = SQLAlchemy(app)
+
+class Pokemon(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True,autoincrement=True)
+    name: Mapped[str] = mapped_column(db.String, nullable=False)
+
+# con esta sentencia se crea las tablas 
+with app.app_context():
+    db.create_all()
+
+
 
 def get_pokemon_data(pokemon):
    url = f'https://pokeapi.co/api/v2/ {pokemon}'
@@ -15,6 +39,23 @@ def home():
 @app.route("/detalle")
 def detalle():
     return render_template('detalle.html')
+
+
+@app.route("/prueba")
+def prueba():
+    new_pokemon = 'Vaporeon'
+    obj = Pokemon(name = new_pokemon)
+    db.session.add(obj)
+    db.session.commit()
+    return 'Pokemon agregado'
+
+@app.route("/busqueda/<name>")
+def busqueda(name):
+    poke = Pokemon.query.filter_by(name=name).first()
+    return str(poke.id)
+    
+
+    return pokemon.id
 
 
 
